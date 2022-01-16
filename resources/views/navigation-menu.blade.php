fc8f071ddc791c5dd855ba38963e438682d9df63
FarLaLook <fluke44412@gmail.com>
2022-01-16 17:25:43 +0700

    Update navigation-menu.blade.php
    Add cat logo to navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <img src="https://cdn.discordapp.com/attachments/703965223316357170/932216797975941190/images.png" alt="Stickman" width="45px", height="45px">
                     </a>
                 </div>
 
