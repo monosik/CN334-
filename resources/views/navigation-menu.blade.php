f70f2153948dfc92a6f42cad288244f38983a7e5
Osawa.ryu <60128482+K656l@users.noreply.github.com>
2022-01-16 23:22:27 +0700

    Change navbar name

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" style="font-family: 'Press Start 2P', cursive; font-size: 15px;">
-                        {{ __('Dashboard') }}
+                        {{ __('Home') }}
                     </x-jet-nav-link>
                 </div>
             </div>
