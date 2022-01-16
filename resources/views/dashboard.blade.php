a930e206251a8e2963b89eb36646fbd6163da6d6
monosik <tot74800@gmail.com>
2022-01-16 18:31:21 +0700

    Change Button color and color heading
    Change all Button color on dashboard page and color heading on dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                 <div class="flex-auto text-2xl mb-4">Tasks List</div>
                 
                 <div class="flex-auto text-right mt-2">
-                    <a href="/task" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Add new Task</a>
+                    <a href="/task" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded" style="background-color: #8BE8CB; ">Add new Task</a>
                 </div>
             </div>
             <table class="w-full text-md rounded mb-4">
@@ -21,13 +21,15 @@
                 <tbody>
                 @foreach(auth()->user()->tasks as $task)
                     <tr class="border-b hover:bg-orange-100">
-                        <td class="p-3 px-5"><a href="/task/{{$task->id}}" name="edit" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-black py-1 px-2 rounded focus:outline-none focus:shadow-outline">Edit</a>
+                        <td class="p-3 px-5"><a href="/task/{{$task->id}}" name="edit" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-black py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                        style="background-color: #f7b267;">Edit</a>
                             {{$task->description}}
                         </td>
                         <td class="p-3 px-5">
                             
                             <form action="/task/{{$task->id}}" class="inline-block">
-                                <button type="submit" name="delete" formmethod="POST" class="text-sm bg-red-500 hover:bg-red-700 text-black py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
+                                <button type="submit" name="delete" formmethod="POST" class="text-sm bg-red-500 hover:bg-red-700 text-black py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                                style="background-color: #ff758f;">Delete</button>
                                 {{ csrf_field() }}
                             </form>
                         </td>
