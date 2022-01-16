0ce5ba484dc3fc40bb89ddb6e0b3be36c18322e2
Osawa.ryu <60128482+K656l@users.noreply.github.com>
2022-01-16 22:30:33 +0700

    Change header color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow" style="background-color: #D7B29D;">
+                <header class="bg-white shadow" style="background-color: #bcdefa;">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
